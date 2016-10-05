6abe183facac2740ffa8c19b2b19f6ccbbc8b30a
gossi <github@gos.si>
2016-10-05 14:46:59 +0200

    Initial package

---
+++ src/EventsModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace iuf\events;
+
+use keeko\framework\foundation\AbstractModule;
+
+/**
+ * Iuf Events
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class EventsModule extends AbstractModule {
+
+	/**
+	 */
+	public function install() {
+	}
+
+	/**
+	 */
+	public function uninstall() {
+	}
+
+	/**
+	 * @param mixed $from
+	 * @param mixed $to
+	 */
+	public function update($from, $to) {
+	}
+}
